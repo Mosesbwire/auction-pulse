88c88ab42dfb4d3f5baa2740af49a7788ec598a8
Mosesbwire <mosesdev92@gmail.com>
2023-12-14 00:12:19 +0300

    feat(database-connection): added configuration to connect to database on app startup

---
+++ config/db.config.ts
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+import AppError from '../src/libraries/error';
+
+/**
+ * makes a connection to the database on application start up
+ * @param url database url connetion string
+ */
+export default async function connectToDb(url: string): Promise<void> {
+	try {
+		await mongoose.connect(url);
+		console.log('Connected to mongoDb');
+	} catch (err) {
+		throw new AppError('DatabaseConnectionError', 'Failed to connect to mongoDB', false);
+	}
+}
