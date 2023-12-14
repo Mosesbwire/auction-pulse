4f6bcc55181c09e178e1442525d5c35808d32e59
Mosesbwire <mosesdev92@gmail.com>
2023-12-15 00:58:07 +0300

    feat: add function to wrap async function, provides a consistent way to call and get data from async function

---
+++ src/libraries/utils/asyncWrapper.ts
@@ -0,0 +1,7 @@
+
+export async function asyncWrapper<T>(promises: Promise<T>): Promise<{data?:T, error?: Error}>{
+	const results:PromiseSettledResult<Awaited<T>>[] = await Promise.allSettled([promises])
+	const [settledResult] = results;
+	if (settledResult.status === 'fulfilled') return {data: settledResult.value};
+	return {error: settledResult.reason};
+}
