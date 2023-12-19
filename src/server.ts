e81d0110fe1972cb242053ccdf16ad9640eda3e3
Mosesbwire <mosesdev92@gmail.com>
2023-12-19 13:27:25 +0300

    feat: add function to start up rdis on startup

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express, {Response, Request, NextFunction} from 'express';
 import dotenv from 'dotenv';
 import connectToDb from '../config/db.config';
+import RedisClient from './libraries/caching/redisCache';
 import { createServer } from 'http';
-import { Server } from 'socket.io';
+import { Server, Socket } from 'socket.io';
 import AppError, { errorHandler } from './libraries/error';
+import { auctioneerApi } from './components/auctioneers'
+import { auctionApi } from './components/auction';
+import AuctionService from './components/auction/auctionService';
 
 dotenv.config({
     path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development.local'
@@ -12,7 +16,9 @@ const PORT = process.env.PORT || 3000;
 const DB_URL = process.env.MONGO_URL || ''
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io:Server = new Server(server);
+
+RedisClient.connectRedis();
 try {
     connectToDb(DB_URL)
 } catch (err) {
@@ -21,29 +27,39 @@ try {
     }
 }
 
-app.use('/', (req, res) => {
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
+app.get('/', (_req, res) => {
     res.send('Api working');
 });
 
-io.on('connection', (socket) => {
-    console.log('Connection established');
-    socket.emit('signin','Welcome to the server');
-});
+
+app.use('/api/v1/auctioneers', auctioneerApi);
+app.use('/api/v1/auctions', auctionApi);
+
+const onConnection = async (socket: Socket) => {
+    new AuctionService(io).runAuction(socket);
+}
+io.on('connection', onConnection);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use(async (err: AppError, req: Request, res: Response, next: NextFunction) => {
+app.use(async (err: AppError, _req: Request, res: Response, _next: NextFunction) => {
     await errorHandler(err, res);
 })
 
 process.on('uncaughtException', (err) => {
+    
     if (err instanceof Error){
         errorHandler(err);
     }
 })
 process.on('unhandledRejection', (reason) =>{
+    
     if (reason instanceof Error){
         errorHandler(reason);
     }
 })
+
 
 server.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
