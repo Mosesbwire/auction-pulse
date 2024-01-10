a2d5a546b2092f4c958edca6ef0fe2f69e219b25
Mosesbwire <mosesdev92@gmail.com>
2024-01-10 19:56:34 +0300

    Add route to access the documentation page

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, {Response, Request, NextFunction} from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
 import connectToDb from '../config/db.config';
 import RedisClient from './libraries/caching/redisCache';
 import { createServer } from 'http';
@@ -7,7 +8,10 @@ import { Server, Socket } from 'socket.io';
 import AppError, { errorHandler } from './libraries/error';
 import { auctioneerApi } from './components/auctioneers'
 import { auctionApi } from './components/auction';
+import { AuctionProcessFactory } from './components/auction/auctionService';
+
 import AuctionService from './components/auction/auctionService';
+import passport from 'passport';
 
 dotenv.config({
     path: process.env.NODE_ENV === 'production' ? '.env' : '.env.development.local'
@@ -18,6 +22,8 @@ const app = express();
 const server = createServer(app);
 const io:Server = new Server(server);
 
+
+
 RedisClient.connectRedis();
 try {
     connectToDb(DB_URL)
@@ -27,29 +33,44 @@ try {
     }
 }
 
+// todo - dotenv configuration setting environment variables should happen first. 
+//create config file export when dotenv is initialized
+import { registerJWTstrategy } from './libraries/authentication';
+
+registerJWTstrategy();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false}));
-
+app.use(passport.initialize());
 app.get('/', (_req, res) => {
     res.send('Api working');
 });
+
+app.get('/api/docs', (_req, res) => {
+    res.render('docs')
+})
 
 
 app.use('/api/v1/auctioneers', auctioneerApi);
 app.use('/api/v1/auctions', auctionApi);
 
+const auctionProcess = AuctionProcessFactory.createProcess(io);
+const auctionService = new AuctionService(io, auctionProcess);
 const onConnection = async (socket: Socket) => {
-    new AuctionService(io).runAuction(socket);
+    auctionService.run(socket);
 }
 io.on('connection', onConnection);
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use(async (err: AppError, _req: Request, res: Response, _next: NextFunction) => {
+   
     await errorHandler(err, res);
 })
 
 process.on('uncaughtException', (err) => {
-    
+   
     if (err instanceof Error){
         errorHandler(err);
     }
