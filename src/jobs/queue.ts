336aa7a08c3ac3436c59bca0a4c688eff5262f43
Mosesbwire <mosesdev92@gmail.com>
2024-01-12 19:04:12 +0300

    feat: add function to run background job to update auction data on auction close

--- src/jobs/queue.ts
+++ src/jobs/queue.ts
@@ -1,15 +1,64 @@
-import Queue  from "bull";
-import liveAuction from "../components/auction/auctionModel";
+import Queue, { Job }  from "bull";
+import Auction from "../components/auction/auction";
+import { asyncWrapper } from "../libraries/utils/asyncWrapper";
 
-export const bidQueue = new Queue('bidProcessor', 'redis://127.0.0.1:6379');
 
-bidQueue.process(liveAuction.updateAuctionBid);
 
-bidQueue.on('completed', (job, result)=> {
-    console.log(`JOB ID:${job.id} result:${result} completed`)
+const jobQueue = new Queue('redis://127.0.0.1:6379');
+
+async function updateAuctionBid(job: Job){
+    const data = job.data
+		const results = await asyncWrapper(Auction.findById(data.id));
+		if (results.error) throw results.error;
+		const auction = results.data;
+		if (auction){
+			auction.bids.push(data.bid);
+			await auction.save();
+		}
+		return Promise.resolve(auction)
+}
+
+async function activateAuction(job: Job){
+	const data = job.data
+	const results = await asyncWrapper(Auction.findById(data.id))
+	if (results.error) throw results.error;
+	const auction = results.data;
+	if (auction){
+		auction.status = 'open';
+		await auction.save()
+	}
+	return Promise.resolve(auction);
+}
+
+async function updateAuctionOnClose(job: Job) {
+	
+	const data = job.data
+	
+	const results = await asyncWrapper(Auction.findById(data.id))
+	if (results.error) throw results.error;
+	const auction = results.data;
+	if (auction) {
+		auction.status = 'closed';
+		auction.winner = data.winner
+		await auction.save()
+	}
+	return Promise.resolve(auction);
+}
+
+jobQueue.on('error', (err) => {
+	console.log('An error occured in job queue: ' + err);
 });
 
-bidQueue.on('error', (err) => {
-    console.log(`in error: ${err}`)
+jobQueue.on('completed', (job, results) => {
+	console.log(`JOB ID ${job.id} is compeleted. Data: ${results}`);
 });
+
+jobQueue.process('bids', updateAuctionBid);
+jobQueue.process('activate auction', activateAuction);
+jobQueue.process('close auction', updateAuctionOnClose);
+
+export default jobQueue
+
+
+
 
